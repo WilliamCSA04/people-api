21b43bb0d86345123913d719a43363b2967217b1
Williamcsa04 <williamcaetano14@gmail.com>
2019-02-04 21:14:40 -0200

    Added comentaries to people_helper.rb

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,9 +1,14 @@
 class PeopleHelper
 
+  # Any method that starts with self, is a static method
+
+  # This method calculates the difference between two dates in years.
+  # The second parameter is optional, if no value is passed to newest, it will use the result of Time.current instead
   def self.years_between_dates(oldest, newest = Time.current)
     ((newest - oldest) / 365).floor
   end
 
+  # This method returns true if the two parameters are equal, false otherwise.
   def self.is_a_valid_age?(new_age, calculated_age)
     new_age == calculated_age
   end
