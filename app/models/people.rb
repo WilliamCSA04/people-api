a8de08237833b0eb3afce4c8c3328233f33359a6
Williamcsa04 <williamcaetano14@gmail.com>
2019-02-04 20:37:00 -0200

    Created method to validate an update at age

--- app/models/people.rb
+++ app/models/people.rb
@@ -3,5 +3,17 @@ class People < ApplicationRecord
     validates :name, :birth_date, presence: true
     validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150 }
     
-    
+    validate :new_age, on: :update
+
+    private
+
+    def new_age
+        if age_changed?
+            present = Time.current
+            datetime_of_birth = present - self.age.years
+            date_of_birth = datetime_of_birth.to_date
+            is_a_valid_age = date_of_birth == self.birth_date.to_date
+            self.errors.add(:age, "Invalid age")
+        end
+    end
 end
