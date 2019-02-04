86b4367022a63594b4bc231517e9ca56cdd5c33b
Williamcsa04 <williamcaetano14@gmail.com>
2019-02-04 21:26:23 -0200

    Added create! method to peoples controller

--- app/controllers/peoples_controller.rb
+++ app/controllers/peoples_controller.rb
@@ -15,4 +15,23 @@ class PeoplesController < ApplicationController
     render(json: peoples.as_json, status: :ok)
   end
 
+  def create!
+    begin
+      people = People.create!(create_params)
+      render(json: people.as_json, status: :ok)
+    rescue => error
+      render_exception(error.message)
+    end
+  end
+
+  private
+
+  def render_exception(message)
+    render(json: {error: message}, status: :unprocessable_entity)
+  end
+
+  def create_params
+    params.permit(:name, :birth_at)
+  end
+
 end
