6bec369194d17b5fb0c98afb42a992d3dd144f35
Williamcsa04 <williamcaetano14@gmail.com>
2019-02-04 21:31:21 -0200

    Added update method to peoples controller

--- app/controllers/peoples_controller.rb
+++ app/controllers/peoples_controller.rb
@@ -33,6 +33,16 @@ class PeoplesController < ApplicationController
     end
   end
 
+  def update
+    id_to_search = params[:id]
+    people = People.find_by_id(id_to_search)
+    if people.update(update_params)
+      render(json: people.as_json, status: :ok)
+    else
+      render_error(people.errors)
+    end
+  end
+
   private
 
   def render_error(message)
@@ -43,4 +53,7 @@ class PeoplesController < ApplicationController
     params.permit(:name, :birth_at)
   end
 
+  def update_params
+    params.permit(:name, :age)
+  end
 end
