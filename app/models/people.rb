dd666e7ae0ad6e7aba7b6a49007dab2c13bfbd4d
Williamcsa04 <williamcaetano14@gmail.com>
2019-02-04 20:41:33 -0200

    Changed all birth_date to birth_at

--- app/models/people.rb
+++ app/models/people.rb
@@ -1,6 +1,6 @@
 class People < ApplicationRecord
 
-    validates :name, :birth_date, presence: true
+    validates :name, :birth_at, presence: true
     validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150 }
     
     validate :new_age, on: :update
@@ -12,7 +12,7 @@ class People < ApplicationRecord
             present = Time.current
             datetime_of_birth = present - self.age.years
             date_of_birth = datetime_of_birth.to_date
-            is_a_valid_age = date_of_birth == self.birth_date.to_date
+            is_a_valid_age = date_of_birth == self.birth_at.to_date
             self.errors.add(:age, "Invalid age")
         end
     end
