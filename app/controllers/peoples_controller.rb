e7047c90b7a73808cd369616c09514f8656a64e2
Williamcsa04 <williamcaetano14@gmail.com>
2019-02-04 21:20:25 -0200

    Created index method for peoples controller

--- app/controllers/peoples_controller.rb
+++ app/controllers/peoples_controller.rb
@@ -1,2 +1,13 @@
 class PeoplesController < ApplicationController
+
+  def index
+    begin
+      id_to_search = params[:id]
+      people = People.find(id_to_search)
+      render(json: people.as_json, status: :ok)
+    rescue => error
+      render(json: {error: error.message}, status: :unprocessable_entity)
+    end
+  end
+
 end
