b6479d433e05c55a7e4c37acc4099600ccbb6ac4
Williamcsa04 <williamcaetano14@gmail.com>
2019-02-05 00:28:15 -0200

    Added tests to Person Helper

--- test/helpers/person_helper_test.rb
+++ test/helpers/person_helper_test.rb
@@ -8,4 +8,20 @@ class PersonHelperTest < ActiveSupport::TestCase
     assert PersonHelper.is_a_valid_age?(a, b)
   end
 
+  test "Should return false if a and b are different" do
+    a = 1
+    b = 0
+    assert_not PersonHelper.is_a_valid_age?(a, b)
+  end
+
+  test "The years_between_dates should return 10" do
+    date = 10.years.ago
+    assert PersonHelper.years_between_dates(date) == 10
+  end
+
+  test "The years_between_dates should return 9" do
+    date = 9.years.ago
+    assert_not PersonHelper.years_between_dates(date) == 10
+  end
+
 end
