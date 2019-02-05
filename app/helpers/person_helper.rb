9be89c4f70ffe92ab6203509a62c859d02548ff9
Williamcsa04 <williamcaetano14@gmail.com>
2019-02-04 23:27:23 -0200

    Changed call to floor method to round

--- app/helpers/person_helper.rb
+++ app/helpers/person_helper.rb
@@ -5,7 +5,7 @@ class PersonHelper
   # This method calculates the difference between two dates in years.
   # The second parameter is optional, if no value is passed to newest, it will use the result of Time.current instead
   def self.years_between_dates(oldest, newest = Time.current)
-    ((newest - oldest) / 1.year).floor
+    ((newest - oldest) / 1.year).round
   end
 
   # This method returns true if the two parameters are equal, false otherwise.
